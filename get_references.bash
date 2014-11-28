c35bb13d58180593e671dae213107e52a62ccb0b
Pat Schloss <pschloss@umich.edu>
2014-11-28 10:25:53 -0500

    Add make commands for getting references

--- get_references.bash
+++ get_references.bash
@@ -1,2 +1,57 @@
-# This command will pull down a copy of the HMP_MOCK.fasta reference sequence file and  the necessary
-# reference databases
+# This command will pull down a copy of the HMP_MOCK.fasta reference sequence
+# file and the necessary reference databases
+
+mkdir references
+cd references
+
+
+# We want the latest greatest reference alignment and the SILVA reference
+# alignment is the best reference alignment on the market. This version is from
+# v119 and described at http://blog.mothur.org/2014/08/08/SILVA-v119-reference-files/.
+# We will use the full-length version of the database, which contains 137,879
+# bacterial sequences. This also contains the reference taxonomy. We will limit
+# the databases to only include bacterial sequences.
+
+wget -N http://www.mothur.org/w/images/2/27/Silva.nr_v119.tgz
+tar xvzf Silva.nr_v119.tgz
+mothur "#get.lineage(fasta=silva.nr_v119.align, taxonomy=silva.nr_v119.tax, taxon=Bacteria)"
+mv silva.nr_v119.pick.align silva.bacteria.fasta
+mv silva.nr_v119.pick.tax silva.bacteria.tax
+touch got.silva
+#silva.bacteria.fasta
+#silva.bacteria.tax
+
+# We also want the greengenes reference taxonomy. This version is from the
+# greengenes v13_8_99 and is described at
+# http://blog.mothur.org/2014/08/12/greengenes-v13_8_99-reference-files/
+
+wget -N http://www.mothur.org/w/images/6/68/Gg_13_8_99.taxonomy.tgz
+tar xvzf Gg_13_8_99.taxonomy.tgz
+touch got.gg
+#gg_13_8_99.fasta
+#gg_13_8_99.gg.tax
+
+# Next, we want the RDP reference taxonomy. The current version is v10 and we
+# use a "special" pds version of the database files, which are
+# described at http://blog.mothur.org/2014/10/28/RDP-v10-reference-files/
+
+wget -N http://www.mothur.org/w/images/2/24/Trainset10_082014.pds.tgz
+tar xvzf Trainset10_082014.pds.tgz
+mv trainset10_082014.pds/* ./
+rmdir trainset10_082014.pds
+touch got.rdp
+#trainset10_082014.pds.fasta
+#trainset10_082014.pds.tax
+
+# Finally, we want to align the mock community reference sequences to our newly
+# created silva.bacteria.fasta file...
+
+mothur "#align.seqs(fasta=HMP_MOCK.fasta, reference=silva.bacteria.fasta)"
+touch got.mock
+#HMP_MOCK.align
+
+# Clean up and move out...
+
+rm README*
+
+cd ../
